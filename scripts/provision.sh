e84d8c9aeecef8054cb55e50236f34b3dee34620
Igor Bari <mucsi96@gmail.com>
2024-05-12 13:30:02 +0200

    simplify ansible configuration

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-export ANSIBLE_CONFIG=ansible.cfg
+# https://docs.ansible.com/ansible/latest/reference_appendices/config.html
+export ANSIBLE_INVENTORY=./inventory
+export ANSIBLE_ROLES_PATH=./roles
+export ANSIBLE_NOCOWS=1
 
 ansible-playbook playbooks/main.yml
