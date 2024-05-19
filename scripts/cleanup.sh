853d8135d13b45e230359eadb3539fdaed76a4c4
Igor Bari <mucsi96@gmail.com>
2024-05-19 10:17:46 +0200

    deploy demo app

--- scripts/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# https://docs.ansible.com/ansible/latest/reference_appendices/config.html
-export ANSIBLE_INVENTORY=./inventory
-export ANSIBLE_ROLES_PATH=./roles
-export ANSIBLE_NOCOWS=1
-
-ansible-playbook playbooks/delete.yml
